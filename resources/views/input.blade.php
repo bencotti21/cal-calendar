911eb60f7fe1f892dc24de9b628dcec70553217b
bencotti21 <bencotti21@gmail.com>
2023-06-27 14:56:49 +0900

    feat: メモ入力画面にエラーメッセージの表示を追加

--- resources/views/input.blade.php
+++ resources/views/input.blade.php
@@ -8,6 +8,7 @@
   <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
       <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <x-input-error class="m-4" :messages="$errors->all()"></x-input-error>
         <div class="p-6 text-gray-900">
           <form method="POST" action="{{ route('memo.store') }}">
             @csrf
